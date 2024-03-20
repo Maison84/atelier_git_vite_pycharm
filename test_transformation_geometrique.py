f5e2f6f7c3c0165172326e482625701d3d87cb1d
maison84 <maisonned84@gmail.com>
2024-03-20 10:10:57 -0400

    calculer_coordonne_clou + test

--- test_transformation_geometrique.py
+++ test_transformation_geometrique.py
@@ -28,7 +28,7 @@ from transformation_geometrique import calculer_rotate_point
 
 def test_rotate_point():
     point_attendu = (-0.27, 4.46)
-    assert calculer_rotate_point((2, 4), 30) == point_attendu, f"Échec du test : le résultat {resultat} ne correspond pas aux valeurs attendues {attendu}."
+    assert calculer_rotate_point((2, 4), 30) == point_attendu, f"Échec du test : le résultat ne correspond pas aux valeurs attendues {point_attendu}."
 
 
 from transformation_geometrique import calculer_inclinaison_point
@@ -37,14 +37,14 @@ def test_calculer_inclinaison_point_1():
     point = (2, 4)
     angle = 5
     direction = 'x'
-    result = calculer_inclinaison_point(point, angle, direction)
-    expected = (2.35, 4.0)
-    assert result == expected,f"Échec du test : le résultat {resultat} ne correspond pas aux valeurs attendues {attendu}."
+    resultat = calculer_inclinaison_point(point, angle, direction)
+    attendu = (2.35, 4.0)
+    assert resultat == attendu,f"Échec du test : le résultat {resultat} ne correspond pas aux valeurs attendues {attendu}."
 
 def test_calculer_inclinaison_point_2():
     point = (2, 4)
     angle = 5
     direction = 'y'
-    result = calculer_inclinaison_point(point, angle, direction)
-    expected = (2.0, 4.17)
-    assert result == expected, f"Échec du test : le résultat {resultat} ne correspond pas aux valeurs attendues {attendu}."
+    resultat = calculer_inclinaison_point(point, angle, direction)
+    attendu = (2.0, 4.17)
+    assert resultat == attendu, f"Échec du test : le résultat {resultat} ne correspond pas aux valeurs attendues {attendu}."
