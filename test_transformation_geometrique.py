8851997069260a362f6a225ff3e0ab604d123db2
hugo <hugo.magonnet.1@ens.etsmtl.ca>
2024-03-22 11:26:21 -0400

    commencement du programme calculer_coordonnees_clou

--- test_transformation_geometrique.py
+++ test_transformation_geometrique.py
@@ -1,37 +1,24 @@
 from transformation_geometrique import calculer_reflexion_point
+from transformation_geometrique import calculer_rotate_point
+from transformation_geometrique import calculer_inclinaison_point
 
 def test_calculer_reflexion_point_1():
-
   point = (2, 4)
   axe = 'x'
-
   resultat = calculer_reflexion_point(point, axe)
-
   attendu = (2, -4)
-
   assert resultat == attendu, f"Échec du test : le résultat {resultat} ne correspond pas aux valeurs attendues {attendu}."
-
 
 def test_calculer_reflexion_point_2():
-
   point = (2, 4)
   axe = 'y'
-
   resultat = calculer_reflexion_point(point, axe)
-
   attendu = (-2, 4)
-
   assert resultat == attendu, f"Échec du test : le résultat {resultat} ne correspond pas aux valeurs attendues {attendu}."
-
-
-from transformation_geometrique import calculer_rotate_point
 
 def test_rotate_point():
     point_attendu = (-0.27, 4.46)
     assert calculer_rotate_point((2, 4), 30) == point_attendu, f"Échec du test : le résultat ne correspond pas aux valeurs attendues {point_attendu}."
-
-
-from transformation_geometrique import calculer_inclinaison_point
 
 def test_calculer_inclinaison_point_1():
     point = (2, 4)
