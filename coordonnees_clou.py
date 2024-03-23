d9262ba174e91dd8c5160e9bb25b23b46e466e71
hugo <hugo.magonnet.1@ens.etsmtl.ca>
2024-03-23 18:03:02 -0400

    réalisation de appliquer_transformation_clou

--- coordonnees_clou.py
+++ coordonnees_clou.py
@@ -10,3 +10,11 @@ def calculer_coordonnes_clou(a, b, c, d, e):
     points = [('pt_0', pt_0), ('pt_1', pt_1), ('pt_2', pt_2), ('pt_3', pt_3), ('pk_2', pk_2), ('pk_0', pk_0),
               ('pk_1', pk_1)]
     return points
+
+def appliquer_transformation_clou(point_clou, center_rotation, angle_rotation, angle_inclinaison, direction_inclinaison, axe_reflexion):
+    # Calcul des coordonnées des points pour chaque transformation
+    reflexion = calculer_reflexion_point(point_clou, axe_reflexion)
+    rotation = calculer_rotate_point(point_clou, angle_rotation, center_rotation)
+    inclinaison = calculer_inclinaison_point(point_clou, angle_inclinaison, direction_inclinaison)
+    return [reflexion], [rotation], [inclinaison]
+
